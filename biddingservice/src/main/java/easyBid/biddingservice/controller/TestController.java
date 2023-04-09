c1e8255dc1a5fa564ab2b27209b55d761ea8cc83
Faruq Afolabi <gbolayo13@gmail.com>
2023-04-09 15:10:40 -0400

    test controller
    Modified test controller to receive item info from auction server

--- biddingservice/src/main/java/easyBid/biddingservice/controller/TestController.java
+++ biddingservice/src/main/java/easyBid/biddingservice/controller/TestController.java
@@ -26,35 +26,37 @@ public class TestController {
         return "timer";
     }
 
-    @GetMapping("/room")
-    public String startRoom(HttpServletRequest request, HttpServletResponse response) {
-
+    @GetMapping("/room/{username}/{itemId}")
+    public String startRoom(HttpServletRequest request, HttpServletResponse response, @PathVariable String username, @PathVariable long itemId) {
+        System.out.println("StartRoom");
         //Faruq  vv
-        String auctionId = (String) request.getSession().getAttribute("itemId");
-        String userId = (String) request.getSession().getAttribute("username");
-        System.out.println("AuctionId: " + auctionId + "Username:" +userId);
+//        String auctionId = (String) request.getSession().getAttribute("itemId");
+//        String userId = (String) request.getSession().getAttribute("username");
+        System.out.println("AuctionId: " + itemId + " Username: " + username);
+
+        request.getSession().setAttribute("username", username);
+        request.getSession().setAttribute("itemId", itemId);
 
         //Aidan  vv
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8090/auction/items/"+auctionId;
+        String url = "http://localhost:8090/auction/items/"+itemId;
         String r = restTemplate.getForObject(url, String.class);
         System.out.println("Response: " + r);
 
-
         return "timer";
     }
 
-    @GetMapping("/room/{id}")
-    public String getRoom(@PathVariable("id") Long auctionId) {
-
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8090/auction/items/"+auctionId;
-        String response = restTemplate.getForObject(url, String.class);
-        System.out.println("Response: " + response);
-
-
-        return "timer";
-    }
+//    @GetMapping("/room/{id}")
+//    public String getRoom(@PathVariable("id") Long auctionId) {
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//        String url = "http://localhost:8090/auction/items/"+auctionId;
+//        String response = restTemplate.getForObject(url, String.class);
+//        System.out.println("Response: " + response);
+//
+//
+//        return "timer";
+//    }
 
 
     @GetMapping("/forwardroom")
